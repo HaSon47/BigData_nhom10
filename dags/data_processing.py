eae0b4685c16cf23875fb3148227d80fe9a2b867
HaSon47 <hachit1k29@gmail.com>
2026-01-11 18:45:34 +0700

    update batch layer

---
+++ dags/data_processing.py
@@ -0,0 +1,108 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import timedelta
+from datetime import date
+
+import pendulum
+
+
+default_args = {
+    'owner': 'jazzdung',
+    'retries':5,
+    'retry_delay': timedelta(minutes=20)
+}
+
+def get_this_week():
+    today = date.today()
+    today_date = today.strftime("%m%Y")
+    week = str(int(today.strftime("%d"))//7 +1)
+    return today_date+ "_" +week
+
+def get_today():
+    today = date.today()
+    today_date = today.strftime("%d%m%Y")
+    return today_date
+
+with DAG(
+    dag_id='data_processing',
+    description='Process data',
+    start_date=pendulum.yesterday(),
+    schedule_interval='0 0 * * *',
+    catchup=True
+) as dag:
+
+    task_create_clean_folder = BashOperator(
+        task_id='create_today_clean_folder',
+        bash_command='HADOOP_USER_NAME=hadoop /home/jazzdung/hadoop/bin/hdfs dfs -mkdir /user/hadoop/clean/{{ params.date }}',
+        params = {'date' : get_today()}
+    )
+
+    task_create_model_folder = BashOperator(
+        task_id='create_today_model_folder',
+        bash_command='HADOOP_USER_NAME=hadoop /home/jazzdung/hadoop/bin/hdfs dfs -mkdir /user/hadoop/model/{{ params.date }}',
+        params = {'date' : get_today()}
+    )
+
+    task_create_vis_folder = BashOperator(
+        task_id='create_today_vis_folder',
+        bash_command='HADOOP_USER_NAME=hadoop /home/jazzdung/hadoop/bin/hdfs dfs -mkdir /user/hadoop/vis/{{ params.date }}',
+        params = {'date' : get_today()}
+    )
+
+    task_grant_clean_access = BashOperator(
+        task_id='grant_clean_access',
+        bash_command='HADOOP_USER_NAME=hadoop /home/jazzdung/hadoop/bin/hdfs dfs -chown -R jazzdung /user/hadoop/clean/{{ params.date }}',
+        params = {'date' : get_today()}
+    )
+
+    task_grant_model_access = BashOperator(
+        task_id='grant_model_access',
+        bash_command='HADOOP_USER_NAME=hadoop /home/jazzdung/hadoop/bin/hdfs dfs -chown -R jazzdung /user/hadoop/model/{{ params.date }}',
+        params = {'date' : get_today()}
+    )
+
+    task_grant_vis_access = BashOperator(
+        task_id='grant_vis_access',
+        bash_command='HADOOP_USER_NAME=hadoop /home/jazzdung/hadoop/bin/hdfs dfs -chown -R jazzdung /user/hadoop/vis/{{ params.date }}',
+        params = {'date' : get_today()}
+    )
+
+    task_clean_shopee_data = BashOperator(
+        task_id='clean_shopee_data',
+        bash_command='python3 /mnt/disk1/hachi/BigData_nhom10/batch/script/shopee_data.py --origin hdfs://viet:9000/user/hadoop/raw/{{ params.week }}/shopee_raw.ndjson --destination hdfs://viet:9000/user/hadoop/clean/{{ params.date }}/shopee_full_data.csv',
+        params = {'week' : get_this_week(), 'date' : get_today()}
+    )
+
+    task_clean_lazada_data = BashOperator(
+        task_id='clean_lazada_data',
+        bash_command='python3 /mnt/disk1/hachi/BigData_nhom10/batch/script/lazada_data.py --origin hdfs://viet:9000/user/hadoop/raw/{{ params.week }}/lazada_raw.ndjson --destination hdfs://viet:9000/user/hadoop/clean/{{ params.date }}/lazada_full_data.csv',
+        params = {'week' : get_this_week(), 'date' : get_today()}
+    )
+
+    task_create_visualize_data = BashOperator(
+        task_id='create_visualize_data',
+        bash_command='python3 /mnt/disk1/hachi/BigData_nhom10/batch/script/visualize_data.py --origin hdfs://viet:9000/user/hadoop/clean/{{ params.date }}/shopee_full_data.csv --destination hdfs://viet:9000/user/hadoop/vis/{{ params.date }}/visualize_data.csv',
+        params = {'date' : get_today()}
+    )
+
+    task_create_model_data = BashOperator(
+        task_id='create_model_data',
+        bash_command='python3 /mnt/disk1/hachi/BigData_nhom10/batch/script/model_data.py --shopee hdfs://viet:9000/user/hadoop/clean/{{ params.date }}/shopee_full_data.csv --lazada hdfs://viet:9000/user/hadoop/clean/{{ params.date }}/lazada_full_data.csv --destination hdfs://viet:9000/user/hadoop/clean/{{ params.date }}/model_data.csv',
+        params = {'date' : get_today()}
+    )
+
+    task_train_model = BashOperator(
+        task_id='train_model',
+        bash_command='python3 /mnt/disk1/hachi/BigData_nhom10/batch/model/model.py --mode train --save_dir hdfs://viet:9000/user/hadoop/model/{{ params.date }} --train_csv_path hdfs://viet:9000/user/hadoop/clean/{{ params.date }}/model_data.csv',
+        params = {'date' : get_today()}
+    )
+
+    task_create_clean_folder >> task_grant_clean_access
+    task_create_model_folder >> task_grant_model_access
+    task_create_vis_folder >> task_grant_vis_access
+    task_grant_clean_access >> task_clean_shopee_data
+    task_grant_clean_access >> task_clean_lazada_data
+    [task_clean_shopee_data, task_grant_vis_access] >> task_create_visualize_data
+    [task_clean_shopee_data, task_clean_lazada_data] >> task_create_model_data
+
+    [task_grant_model_access, task_create_model_data] >> task_train_model
--- main.py
+++ main.py
@@ -1,7 +1,71 @@
+import logging
+import argparse
+from crawler.lazada_scraper import LazadaScraper
 from crawler.shopee_scraper import ShopeeScraper
 
-scraper = ShopeeScraper(num_page_to_scrape=1, is_headless=False, data_dir=r'C:\Users\Lenovo\Desktop\20251\BigData\code\BigData_nhom10\data\shopee')
-# if login = run browser in the background
-scraper.get_main_page()
-scraper.quit()
-#scraper.get_product_urls()
+
+import os
+os.chdir(os.path.dirname(__file__))
+
+logger = logging.getLogger('scraper')
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+fh = logging.FileHandler('log.txt')
+fh.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+if __name__ == '__main__':
+    # Instantiate the parser
+    parser = argparse.ArgumentParser(description='E-commerce data scraper')
+
+    parser.add_argument('--site', type=str,
+                        help='The website to scrape from. Values can be lazada or shopee')
+    parser.add_argument('--type', type=str,
+                        help='What kind of data to scrape. Values can be url or info')
+    parser.add_argument('--num_page', type=int,
+                        help='How many pages to scrape urls from. Only effective when type is url')
+    parser.add_argument('--consumer_id', type=str,
+                        help='The client id of the consumer. Only effective when type is info')
+    parser.add_argument('--headless', action='store_true',
+                        help='Run browser in the background')
+    parser.add_argument('--login', action='store_true',
+                        help='Run browser in the background')
+
+    args = parser.parse_args()
+
+    if args.site == 'shopee':
+        if args.num_page:
+            scraper = ShopeeScraper(num_page_to_scrape=args.num_page, is_headless=args.headless, consumer_id=args.consumer_id)
+        else:
+            scraper = ShopeeScraper(is_headless=args.headless, consumer_id=args.consumer_id)
+
+        if args.login:
+            scraper.get_main_page()
+
+        if args.type == "url":
+            scraper.get_product_urls()
+        elif args.type == "info":
+            scraper.get_product_info()
+
+    elif args.site == 'lazada':
+        if args.num_page:
+            scraper = LazadaScraper(num_page_to_scrape=args.num_page, is_headless=args.headless, consumer_id=args.consumer_id)
+        else:
+            scraper = LazadaScraper(is_headless=args.headless, consumer_id=args.consumer_id)
+
+        if args.login:
+            scraper.get_main_page()
+
+        if args.type == "url":
+            scraper.get_product_urls()
+        elif args.type == "info":
+            scraper.get_product_info()
